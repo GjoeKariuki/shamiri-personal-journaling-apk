2c762ad7fec19391a51b2b484d3c5af68a834d8d
GjoeKariuki <githaigageorge12@gmail.com>
2024-07-05 15:31:10 +0300

    navigation

--- components/Navigator.tsx
+++ components/Navigator.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import AddJournalpage from "./AddJournalpage";
+import EditJournalpage from "./EditJournalpage";
+import Homepage from "./Homepage";
+import Loginpage from "./Loginpage";
+import Profilepage from "./Profilepage";
+import Registrationpage from "./Registrationpage";
+
+const StackNav = createNativeStackNavigator();
 
 function Navigator() {
-  return <div>Navigator</div>;
+  return (
+    <NavigationContainer>
+      <StackNav.Navigator initialRouteName="Login">
+        <StackNav.Screen name="Register" component={Registrationpage} />
+        <StackNav.Screen name="Login" component={Loginpage} />
+        <StackNav.Screen name="Home" component={Homepage} />
+        <StackNav.Screen name="AddJournal" component={AddJournalpage} />
+        <StackNav.Screen name="EditJournal" component={EditJournalpage} />
+        <StackNav.Screen name="Profile" component={Profilepage} />
+      </StackNav.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default Navigator;
